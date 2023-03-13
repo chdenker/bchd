e1871b86d8e7fad491400a765102b47b2fb62dfc
Christopher Denker <mail@chrisden.de>
2023-03-14 00:36:50 +0100

    Implement open, release, read, write similarly to the scull driver

--- bchd.c
+++ bchd.c
@@ -29,17 +29,103 @@ MODULE_AUTHOR("Christopher Denker");
 MODULE_DESCRIPTION("Basic character device");
 MODULE_LICENSE("GPL");
 
+#ifndef BCHD_QUANTUM
+#define BCHD_QUANTUM    4000
+#endif
+
+#ifndef BCHD_QSET
+#define BCHD_QSET       1000
+#endif
+
 int bchd_major = 0;    /* we use a dynamic major by default */
 int bchd_minor = 0;
+int bchd_quantum_size = BCHD_QUANTUM;
+int bchd_qset_size = BCHD_QSET;
+
+/*
+ * The data of a bchd device is represented using a linked list.
+ * Each list item contains an array of pointers, called quantum set,
+ * where each pointer points to a memory area, called a quantum.
+ * The sizes of a quantum set and a quantum are attributes of the bchd_dev struct.
+ */
+struct bchd_qset {
+    void **data;
+    struct bchd_qset *next;
+};
 
 struct bchd_dev {
-    struct cdev cdev;   /* Char device structure */
+    struct bchd_qset *data;     /* Pointer to first quantum set */
+    int quantum_size;           /* Amount of bytes per quantum */
+    int qset_size;              /* Amount of pointers in a quantum set */
+    unsigned long size;         /* Amount of data (in bytes) stored here */
+    struct mutex lock;          /* Mutual exclusion semaphore */
+    struct cdev cdev;           /* Char device structure */
 };
 
 struct bchd_dev *bchd_dev; /* allocated in bchd_init */
 
+
+/*
+ * Empty out the bchd device.
+ * Here, we walk through the entire list and free any quantum and quantum sets we find.
+ *
+ * NOTE:
+ *  -- Device semaphore must be held
+ *  -- We assume dev != NULL
+ */
+void bchd_trim(struct bchd_dev *dev)
+{
+    struct bchd_qset *next, *dptr;
+    int qset_size = dev->qset_size;
+    int i;
+
+    /* Iterate over all list items and free them */
+    for (dptr = dev->data; dptr != NULL; dptr = next) {
+        if (dptr->data != NULL) {
+            /* Free all quanta */
+            for (i = 0; i < qset_size; i++) {
+                kfree(dptr->data[i]);
+            }
+            kfree(dptr->data);
+            dptr->data = NULL;
+        }
+        next = dptr->next;
+        kfree(dptr);
+    }
+
+    dev->size = 0;
+    dev->quantum_size = bchd_quantum_size;
+    dev->qset_size = bchd_qset_size;
+    dev->data = NULL;
+}
+
 int bchd_open(struct inode *inode, struct file *filp)
 {
+    struct bchd_dev *dev;
+
+    /*
+     * The i_cdev field of inode contains the cdev structure we set up before.
+     * However, we want the bchd_dev struct that contains this cdev struct.
+     */
+    dev = container_of(inode->i_cdev, struct bchd_dev, cdev);
+
+    /* We use this in bchd_read and bchd_write to obtain the bchd_dev struct. */
+    filp->private_data = dev;
+
+    /*
+     * Trim the length of the device to 0 if open was write only.
+     * We do this since overwriting a bchd device with a shorter file
+     * results in a shorter device data area.
+     * This does nothing if the device is opened for reading.
+     */
+    if ( (filp->f_flags & O_ACCMODE) == O_WRONLY) {
+        if (mutex_lock_interruptible(&dev->lock)) {
+            return -ERESTARTSYS;
+        }
+        bchd_trim(dev);
+        mutex_unlock(&dev->lock);
+    }
+
     return 0;
 }
 
@@ -48,14 +134,147 @@ int bchd_release(struct inode *inode, struct file *filp)
     return 0;
 }
 
+/*
+ * Follow the list to the index n and return a pointer to the corresponding item.
+ * This procedure creates new items if necessary.
+ */
+struct bchd_qset * bchd_follow(struct bchd_dev *dev, int n)
+{
+    struct bchd_qset *qs = dev->data;
+
+    /* Allocate first qset if necessary */
+    if (qs == NULL) {
+        qs = dev->data = kmalloc(sizeof(*qs), GFP_KERNEL);
+        if (qs == NULL) {
+            return NULL;
+        }
+        memset(qs, 0, sizeof(*qs));
+    }
+
+    /* Then follow the list */
+    while (n--) {
+        if (qs->next == NULL) {
+            qs->next = kmalloc(sizeof(*qs->next), GFP_KERNEL);
+            if (qs->next == NULL) {
+                return NULL;
+            }
+            memset(qs->next, 0, sizeof(*qs->next));
+        }
+        qs = qs->next;
+    }
+
+    return qs;
+}
+
 ssize_t bchd_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
-    return 0;
+    struct bchd_dev *dev = filp->private_data;
+    struct bchd_qset *dptr;     /* first list item */
+    int quantum_size = dev->quantum_size;
+    int qset_size = dev->qset_size;
+    int item_size = quantum_size * qset_size;
+    int item, qset_pos, q_pos, rest;
+    ssize_t retval = 0;
+
+    if (mutex_lock_interruptible(&dev->lock)) {
+        return -ERESTARTSYS;
+    }
+    if (*f_pos >= dev->size) {
+        goto out;
+    }
+    if (*f_pos + count > dev->size) {
+        count = dev->size - *f_pos;
+    }
+
+    /* Find list item, qset index and quantum index (i.e. offset in the quantum) */
+    item = (long) *f_pos / item_size;
+    rest = (long) *f_pos % item_size;
+    qset_pos = rest / quantum_size;
+    q_pos = rest % quantum_size;
+
+    /* Follow the list up to the right position */
+    dptr = bchd_follow(dev, item);
+
+    if (dptr == NULL || dptr->data == NULL || dptr->data[qset_pos] == NULL) {
+        goto out; /* We do not fill holes */
+    }
+
+    /* Read only up to the end of this quantum */
+    if (count > quantum_size - q_pos) {
+        count = quantum_size - q_pos;
+    }
+
+    if (copy_to_user(buf, dptr->data[qset_pos] + q_pos, count)) {
+        retval = -EFAULT;
+        goto out;
+    }
+    *f_pos += count;
+    retval = count;
+
+out:
+    mutex_unlock(&dev->lock);
+    return retval;
 }
 
 ssize_t bchd_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
 {
-    return 0;
+    struct bchd_dev *dev = filp->private_data;
+    struct bchd_qset *dptr;     /* first list item */
+    int quantum_size = dev->quantum_size;
+    int qset_size = dev->qset_size;
+    int item_size = quantum_size * qset_size;
+    int item, qset_pos, q_pos, rest;
+    ssize_t retval = -ENOMEM;  /* value used in "goto out" statements */
+
+    if (mutex_lock_interruptible(&dev->lock)) {
+        return -ERESTARTSYS;
+    }
+
+    /* Find list item, qset index and quantum index (i.e. offset in the quantum) */
+    item = (long) *f_pos / item_size;
+    rest = (long) *f_pos % item_size;
+    qset_pos = rest / quantum_size;
+    q_pos = rest % quantum_size;
+
+    /* Follow the list up to the right position */
+    dptr = bchd_follow(dev, item);
+    if (dptr == NULL) {
+        goto out;
+    }
+    if (dptr->data == NULL) {
+        dptr->data = kmalloc(qset_size * sizeof(char *), GFP_KERNEL);
+        if (dptr->data == NULL) {
+            goto out;
+        }
+        memset(dptr->data, 0, qset_size * sizeof(char *));
+    }
+    if (dptr->data[qset_pos] == NULL) {
+        dptr->data[qset_pos] = kmalloc(quantum_size, GFP_KERNEL);
+        if (dptr->data[qset_pos] == NULL) {
+            goto out;
+        }
+    }
+
+    /* Write only up to the end of this quantum */
+    if (count > quantum_size - q_pos) {
+        count = quantum_size - q_pos;
+    }
+
+    if (copy_from_user(dptr->data[qset_pos] + q_pos, buf, count)) {
+        retval = -EFAULT;
+        goto out;
+    }
+    *f_pos += count;
+    retval = count;
+
+    /* Update the size */
+    if (dev->size < *f_pos) {
+        dev->size = *f_pos;
+    }
+ 
+out:
+    mutex_unlock(&dev->lock);
+    return retval;
 }
 
 struct file_operations bchd_fops = {
@@ -87,6 +306,7 @@ static void bchd_cleanup(void)
 
     /* get rid of char dev entry */
     if (bchd_dev != NULL) {
+        bchd_trim(bchd_dev);
         cdev_del(&bchd_dev->cdev);
         kfree(bchd_dev);
     }
@@ -101,7 +321,8 @@ static int __init bchd_init(void)
 {
     int result;
     dev_t dev = 0;
-    
+
+    /* Obtain device number */    
     result = alloc_chrdev_region(&dev, bchd_minor, 1, "bchd");
     bchd_major = MAJOR(dev);
     if (result < 0) {
@@ -118,6 +339,9 @@ static int __init bchd_init(void)
     memset(bchd_dev, 0, sizeof(*bchd_dev));
 
     /* Initialize the device */
+    bchd_dev->quantum_size = bchd_quantum_size;
+    bchd_dev->qset_size = bchd_qset_size;
+    mutex_init(&bchd_dev->lock);
     bchd_setup_cdev(bchd_dev);
 
     printk(KERN_INFO "bchd: initialized -- device major: %d; device minor: %d\n", MAJOR(dev), MINOR(dev));
